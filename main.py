0e8c6c33274a1aed4cdd01c8fc998a4878b31cfe
NguyenHongAnh <spinkbrotherofficial@gmail.com>
2025-08-11 13:12:06 +0700

    Building home and GET POST req

--- main.py
+++ main.py
@@ -8,8 +8,38 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "asdjahdas"
 socketio = SocketIO(app)
 
+room = {}
+
+def generate_unique_code(length):
+    while True:
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+        if code not in room:
+            break
+
 @app.route("/",methods = ["POST" , "GET"])
 def home():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="Please enter a name.", code = code, name = name)
+        if join != False and not code:
+            return render_template("home.html", error="Please enter a room code", code = code, name = name)
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messenges ": []}
+        elif code not in room:
+            return render_template("home.html", error="Room does not exist", code = code, name = name)
+
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
     return render_template("home.html")
 
 if __name__ == "__main__":
