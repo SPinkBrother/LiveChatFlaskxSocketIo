71bc558643f1b795525d92eabb992e40036dd32d
NguyenHongAnh <spinkbrotherofficial@gmail.com>
2025-08-16 11:40:28 +0700

    Sending messages

--- main.py
+++ main.py
@@ -55,6 +55,21 @@ def room():
         return redirect(url_for("home"))
     return render_template("room.html", code=room)
 
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+    content = { 
+        "name" : session.get("name"),
+        "message" : data["data"]
+
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+
 @socketio.on("connect")
 def connect(auth):
     # get the current session or in the session
